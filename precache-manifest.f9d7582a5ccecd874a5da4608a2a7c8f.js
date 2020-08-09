297020fede1e6880f7cb218536f59da1ff44cb12
darkrider2109 <sanjeevkumarv2.0@gmail.com>
2020-08-09 21:10:03 +0530

    Updates

---
+++ precache-manifest.f9d7582a5ccecd874a5da4608a2a7c8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b870017b38c81c66a61fa62f3a04bad5",
+    "url": "/tooltip/index.html"
+  },
+  {
+    "revision": "37c772aa447b9fe85c3b",
+    "url": "/tooltip/static/css/main.18038029.chunk.css"
+  },
+  {
+    "revision": "67c5498004da85a9d4c8",
+    "url": "/tooltip/static/js/2.81dcfc37.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tooltip/static/js/2.81dcfc37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37c772aa447b9fe85c3b",
+    "url": "/tooltip/static/js/main.45d9b19e.chunk.js"
+  },
+  {
+    "revision": "4a55e7f4368d1c67dda1",
+    "url": "/tooltip/static/js/runtime-main.197a875a.js"
+  }
+]);
